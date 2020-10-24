48e00a8540272760add3f875ffdaa21053fe8ff1
pythonstudent01 <python-student@den01.co.uk>
2020-10-24 10:28:01 +0000

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    
+    #input_string = ""
+    #while input_string != "no":
+    #  input_string = input("Carry on?")
+
+    while True:
+      input_string = input("Carry on?")
+      if input_string == "no":
+        break
 
 if __name__ == '__main__':
     main()
